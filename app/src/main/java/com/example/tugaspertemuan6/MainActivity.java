c775a0d50191cca80460f8f990f523ed6132e349
unknown <vionikaemalia.25@gmail.com>
2021-11-28 22:17:07 +0700

    first commit

---
+++ app/src/main/java/com/example/tugaspertemuan6/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.tugaspertemuan6;
+
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.ExistingWorkPolicy;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final OneTimeWorkRequest request = new
+                OneTimeWorkRequest.Builder(MyWorker.class).build();
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WorkManager.getInstance().enqueueUniqueWork("Notifikasi",
+                        ExistingWorkPolicy.REPLACE,
+                        request);
+            }
+        });
+    }
+}
